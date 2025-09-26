fd44f595780e546a5aaa20d93aad16d13889cb91
Duojia He <ehe@smith.edu>
2025-09-26 03:11:25 -0400

    A3 submission

--- Conversation.java
+++ Conversation.java
@@ -1,27 +1,56 @@
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.Scanner;
+import java.lang.Math;
 
 class Conversation implements ConversationRequirements {
 
   // Attributes 
+  int rounds;
+  String[] chatResponse = {"mhmm, I see!", "ohh okay!", "nice!", "cool!"};
+  String[] welcomeMessage = {"Hi there! What's on your mind?", "Welcome! What's up?", "Hey! How are you?", "Howdy! How's it going?"};
+  String[] endMessage = {"Thanks for chatting!", "Talk to you later!", "Bye, have a good day!", "Bye!"};
+  ArrayList<String> transcript;
 
   /**
    * Constructor 
    */
-  Conversation() {
-    
+  public Conversation(int rnd) {
+    this.rounds = rnd;
+    this.transcript = new ArrayList<>();
   }
 
   /**
    * Starts and runs the conversation with the user
    */
   public void chat() {
+    Scanner scan = new Scanner(System.in);
+    Random r = new Random();
+    int rand = r.nextInt(4);
 
+    System.out.println(welcomeMessage[rand]);
+    this.transcript.add(welcomeMessage[rand]);
+    
+    for (int i = 0; i < this.rounds; i++){
+      String userResponse = scan.nextLine();
+      this.transcript.add(userResponse);
+
+      System.out.println(this.respond(userResponse));
+    }
+
+    System.out.println(endMessage[rand]);
+    this.transcript.add(endMessage[rand]);
   }
 
   /**
    * Prints transcript of conversation
    */
-  public void printTranscript() {
 
+  public void printTranscript() {
+    System.out.println("\n----TRANSCRIPT: ----");
+    for (int i = 0; i < this.transcript.size(); i++){
+      System.out.println(this.transcript.get(i));
+    }
   }
 
   /**
@@ -30,13 +59,43 @@ class Conversation implements ConversationRequirements {
    * @return mirrored or canned response to user input  
    */
   public String respond(String inputString) {
-    String returnString = ""; 
+    String returnString = "";
+    Random r = new Random();
+    int rand = r.nextInt(4);
+
+    // I know this is not the right way to go about it
+    if (!inputString.contains("I ") && !inputString.contains(" me ") && !inputString.contains(" am ") && !inputString.contains(" you ") && !inputString.contains(" my ") && !inputString.contains(" your ")){
+      returnString = chatResponse[rand]; 
+      this.transcript.add(chatResponse[rand]);
+    } else if (inputString.contains("I ")){
+      returnString = inputString.replaceAll("I ", " you ");
+      this.transcript.add(returnString);
+    } else if (inputString.contains(" me ")) {
+      returnString = inputString.replaceAll(" me ", " you ");
+      this.transcript.add(returnString);
+    } else if (inputString.contains(" am ")) {
+      returnString = inputString.replaceAll(" am ", " are ");
+      this.transcript.add(returnString);
+    } else if (inputString.contains(" you ")) {
+      returnString = inputString.replaceAll(" you ", " I ");
+      this.transcript.add(returnString);
+    } else if (inputString.contains(" my ")) {
+      returnString = inputString.replaceAll(" my ", " your ");
+      this.transcript.add(returnString);
+    } else if (inputString.contains(" your ")) {
+      returnString = inputString.replaceAll(" your ", " my ");
+      this.transcript.add(returnString);
+    }
+    
     return returnString; 
   }
 
   public static void main(String[] arguments) {
+    Scanner scan = new Scanner(System.in);
+    System.out.println("How many rounds? (input integer)");
+    int rounds = scan.nextInt();
 
-    Conversation myConversation = new Conversation();
+    Conversation myConversation = new Conversation(rounds);
     myConversation.chat();
     myConversation.printTranscript();
 
--- reflection.md
+++ reflection.md
@@ -1,7 +1,24 @@
 Use this file to record your reflection on this assignment.
 
 - Which classmates did you consult with while working on this assignment?
+    -Abby: talking about the assignment with her helped get my thinking process started, we were able to bounce ideas off of each other
+    -Nicole: helped me with seeing how the "rounds" variable could be both a parameter in the constructor and a user-entered value
+
 - Which session(s) of TA / office hours did you attend?
+    -Sun 7-8: TAs helped me with getting started and helped with setting up the constructor
+
 - What are your initial impressions of Java? 
+    -I have a lot more experience with java than with python, so I don't really have any initial impressions.
+
 - Can you draw any conclusion about programming in general from the similarities or the differences between the two languages? 
+    -Python is a lot more intuitive and less about knowing the syntax
+    -Java has stricter syntax rules and you have remember more things such as adding the semicolon and curly brackets for methods
+        -I like how in Java, the brackets visually contain the method 
+    -printing in Java is kind of annoying, but the short cut makes it a lot better
+    -because I'm more used to java, I prefer it at the current moment. But, I feel like once I am used to python, I will prefer the simpler syntax of python.
+    -There are many similararities between how the two languages work, they just have different ways of doing it
+        -ex: you make a "toString()" method in Java, but in python it is "__self__()"
+        -ex: in Java you use "this" while in python you use "self"
+
 - What worked, what didn't, what advice would you give someone taking this course in the future?
+    -I definitely procrastinated too much on this assignment. I tend to really struggle with meeting deadlines, and I think the idea of having extensions and such makes me procrastinate even more. So in the future I need to pretend like I don't have the option of using an extension to motivate me to actually work on the assignment ahead of time.
